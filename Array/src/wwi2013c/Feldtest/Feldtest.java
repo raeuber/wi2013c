0f3c4ca114b2bf95ed06de7161298f27eb65a51f
Marcus <Marcus@141.31.71.121>
2013-11-05 10:25:12 +0100

    Static, single Methode

--- Array/src/wwi2013c/Feldtest/Feldtest.java
+++ Array/src/wwi2013c/Feldtest/Feldtest.java
@@ -10,11 +10,20 @@ public class Feldtest {
 		for (String Tag : Wochentag) {
 			System.out.println(Tag);
 		}
-		//for
+		/*****
+		 * 
+		 * Feldtest Methode getFeld1
+		 * 
+		 *****/
 		for (int i= 1; i< Wochentag.length; i++){
 			System.out.println(Wochentag[i-1]);
 		}
-		//while
+		
+		/********
+		 * 
+		 * Feldtest Methode getFeld2
+		 * 
+		 ******/
 		int x = 0;
 		while(x <Wochentag.length){
 			System.out.println(Wochentag[x]);
